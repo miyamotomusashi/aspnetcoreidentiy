c8dbce877540ec55c5ef1d083be5d70bff8c20c2
Saadettin Ördekçi <cenbek03@hotmail.com>
2024-09-19 22:16:35 +0300

    Add project files.

---
+++ CoreIdentity/Models/Identity/AppRole.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CoreIdentity.Models.Identity
+{
+  public class AppRole : IdentityRole
+  {
+        public DateTime CreatedOn { get; set; }
+    }
+}
---
+++ CoreIdentity/Models/Identity/AppUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CoreIdentity.Models.Identity
+{
+  public enum Gender { Unknown, Male, Female }
+  public class AppUser : IdentityUser
+  {
+        public Gender Gender { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
